962593fa050ce65109be6c378234ce8c91920ac7
Dalmanski <jayraldjohndalman@gmail.com>
2025-02-23 23:30:19 +0800

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const closeBtn = document.getElementById("closeImg");
 const images = document.querySelectorAll('.image-section img');
 
 images.forEach((img) => {
-    img.onclick = function() {
+    img.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
     }
 });
 
-closeBtn.onclick = function() {
+closeBtn.onclick = function () {
     modal.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
@@ -30,24 +30,24 @@ const closeLogin = document.getElementById("closeLogin");
 const closeSignup = document.getElementById("closeSignup");
 const signupLink = document.getElementById("signupLink");
 
-loginBtn.onclick = function() {
+loginBtn.onclick = function () {
     loginModal.style.display = "block";
 };
 
-closeLogin.onclick = function() {
+closeLogin.onclick = function () {
     loginModal.style.display = "none";
 };
 
-signupLink.onclick = function() {
+signupLink.onclick = function () {
     signupModal.style.display = "block";
-    loginModal.style.display = "none"; 
+    loginModal.style.display = "none";
 };
 
-closeSignup.onclick = function() {
+closeSignup.onclick = function () {
     signupModal.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == loginModal) {
         loginModal.style.display = "none";
     }
@@ -74,24 +74,28 @@ function toggleHeader() {
 
 document.addEventListener("DOMContentLoaded", () => {
     const homeLink = document.getElementById('homeLink');
-    const articleLink = document.getElementById('articleLink');
+    const myLifeLink = document.getElementById('myLifeLink');
+    // const life2024Link = document.getElementById('life2024Link');
     const aboutLink = document.getElementById('aboutLink');
 
     const homeSection = document.getElementById('homeSection');
-    const articleSection = document.getElementById('articleSection');
+    const myLifeSection = document.getElementById('myLifeSection');
+    // const life2024Section = document.getElementById('life2024Section');
     const aboutSection = document.getElementById('aboutSection');
 
     // Function to show a specific section and hide others
     function showSection(sectionToShow) {
         homeSection.style.display = 'none';
-        articleSection.style.display = 'none';
+        myLifeSection.style.display = 'none';
+        // life2024Section.style.display = 'none';
         aboutSection.style.display = 'none';
 
         sectionToShow.style.display = 'block';
     }
 
     homeLink.addEventListener('click', () => showSection(homeSection));
-    articleLink.addEventListener('click', () => showSection(articleSection));
+    myLifeLink.addEventListener('click', () => showSection(myLifeSection));
+    // life2024Link.addEventListener('click', () => showSection(life2024Section));
     aboutLink.addEventListener('click', () => showSection(aboutSection));
 
     showSection(homeSection);
